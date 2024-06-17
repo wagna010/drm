ef083aad58bfedecc4c91e1c8a73154192228459
wagna010 <56608142+wagna010@users.noreply.github.com>
2024-06-16 23:08:25 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+
+#!/bin/bash
+
+# Tornar o arquivo executável
+chmod +x /home/DRMStuff/o11
+
+# Criar o arquivo de serviço systemd
+cat <<EOF > /etc/systemd/system/ott-o11-panel.service
+[Unit]
+Description=OTT O11 PANEL
+
+[Service]
+User=root
+WorkingDirectory=/home/DRMStuff
+ExecStart=/home/DRMStuff/o11 -p 2095 # A porta pode ser alterada conforme sua preferência.
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Recarregar os arquivos de serviço do systemd
+systemctl daemon-reload
+
+# Habilitar o serviço para iniciar automaticamente
+systemctl enable ott-o11-panel.service
+
+# Iniciar o serviço
+systemctl start ott-o11-panel.service
+
+# Exibir mensagem de conclusão da instalação
+echo "Instalação concluída."
